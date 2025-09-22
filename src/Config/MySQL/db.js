e75f2d61cd17a91a70fc1c109aa3ec630396cd9e
abelmuro <abelmuro93@gmail.com>
2025-09-21 17:58:01 -0700

    comit

--- src/Config/MySQL/db.js
+++ src/Config/MySQL/db.js
@@ -1,7 +1,7 @@
 const mysql = require('mysql2');
 
 const pool = mysql.createPool({
-        host: '10.0.0.248',                       // Administration -> Client Connections -> Copy the host
+        host: '73.92.75.163',                       // Administration -> Client Connections -> Copy the host
         port: 3306,
         user: 'root',                            // Administration -> Users and Privileges -> Copy the user name
         password: 'Darkness33!',                 // Use the password that you entered when you installed mySQL
